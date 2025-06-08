0c9ca5ef44ba9bdf73a69df6a8bc1482253b1678
whwnddml <junny4u@gmail.com>
2025-06-08 13:09:45 +0900

    Fix build process: Remove next export and appDir option

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -10,14 +10,9 @@ const nextConfig = {
   trailingSlash: true,
   assetPrefix: '/easy-memo/',
   distDir: 'out',
-  experimental: {
-    appDir: true,
-  },
   env: {
     NEXT_PUBLIC_BASE_PATH: '/easy-memo',
-  },
-  // PWA 설정은 배포 성공 후 추가
-  // 현재는 정적 사이트 배포에 집중
+  }
 }
 
 module.exports = nextConfig 
